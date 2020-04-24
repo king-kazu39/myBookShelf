7fd3f5e6ddb7c8965fc95f41396fdfb6f088712d
Kazuya Nakazato <magmanakazato@gmail.com>
2020-04-24 21:52:48 +0900

    Initial Commit

---
+++ myBookShelf/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myBookShelf
+//
+//  Created by 仲里和也 on 2020/04/24.
+//  Copyright © 2020 仲里和也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
