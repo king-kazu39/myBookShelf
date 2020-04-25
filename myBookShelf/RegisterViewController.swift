0e7d5042179b0fb9b5a4a7395e40ce72c4587cf0
Kazuya Nakazato <magmanakazato@gmail.com>
2020-04-25 17:45:15 +0900

    fix #3 splash画面の後にUITabBarControllerに変更

--- myBookShelf/RegisterViewController.swift
+++ myBookShelf/RegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  HomeViewController.swift
+//  RegisterViewController.swift
 //  myBookShelf
 //
 //  Created by 仲里和也 on 2020/04/25.
@@ -8,27 +8,22 @@
 
 import UIKit
 
-/*
- [Swift 5]RootViewControllerを適用する備忘録
- https://qiita.com/Riscait/items/29e34d922dad834106da
- */
+class RegisterViewController: UIViewController {
 
-// final:RootVCクラスを継承したクラスを作ることを禁止します
-class HomeViewController: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         self.navigationItem.title = "myBookShelf"
         
         let rect = CGRect(x: 0, y: 0, width: 200, height: 200)
         let label = UILabel(frame: rect)
         label.center = self.view.center
-        label.text = "FirstViewController"
+        label.text = "SecondViewController"
         label.textColor = UIColor.black
         label.font = UIFont(name: "HiraKakuProN-W6", size: 17)
         view.backgroundColor = .white
         self.view.addSubview(label)
-        
     }
     
+
 }
