9098701adce67dada628d4efeba68e6e53da384f
Kazuya Nakazato <magmanakazato@gmail.com>
2020-04-25 14:43:25 +0900

    スプラッシュ画面作成

--- myBookShelf/HomeViewController.swift
+++ myBookShelf/HomeViewController.swift
@@ -8,23 +8,38 @@
 
 import UIKit
 
+/*
+ [Swift 5]RootViewControllerを適用する備忘録
+ https://qiita.com/Riscait/items/29e34d922dad834106da
+ */
+
+// final:RootVCクラスを継承したクラスを作ることを禁止します
 class HomeViewController: UIViewController {
 
+//    private var current: UIViewController
+//    
+//    init() {
+//        current = SplashVC()
+//        super.init(nibName:nil, bundle: nil)
+//    }
+//    
+//    required init(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let rect = CGRect(x: 0, y: 0, width: 200, height: 200)
+        let label = UILabel(frame: rect)
+        label.center = self.view.center
+        label.text = "Hello Swift"
+        label.textColor = UIColor.black
+        label.font = UIFont(name: "HiraKakuProN-W6", size: 17)
+        view.backgroundColor = .white
+        self.view.addSubview(label)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- myBookShelf/ViewController.swift
+++ myBookShelf/ViewController.swift
@@ -8,12 +8,62 @@
 
 import UIKit
 
+/*
+Twitterのような動くスプラッシュ画面をふろぐんで作ってみた
+http://developers.goalist.co.jp/entry/2017/07/20/170748
+*/
+
 class ViewController: UIViewController {
 
+    var imageView: UIImageView!
+    private var nextVC: UIViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 実際の表示座標は後で指定する
+        let rect = CGRect(x: 0, y: 0, width: 200, height: 200)
+        imageView = UIImageView(frame: rect)
+    
+        // イメージの表示モード：
+        // scaleAspectFit:画像の縦横サイズを保ったまま、イメージビューに全体が収まるようにフィットさせる
+        imageView.contentMode = .scaleAspectFit
+        imageView.center = self.view.center
+        imageView.image = UIImage(named: "book-icon")
+        view.backgroundColor = .white
+        self.view.addSubview(imageView)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 80%まで縮小させる
+        UIView.animate(withDuration: 0.3,
+                       delay: 1.0,
+                       options: .curveEaseOut,
+                       animations:{ () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)},
+                       completion:{ (Bool) in
+        })
+        
+        // 8倍まで拡大
+        UIView.animate(withDuration: 0.2,
+                       delay: 1.3,
+                       options: .curveEaseInOut,
+                       animations: { () in
+                        self.imageView.transform = CGAffineTransform(scaleX: 8.0, y: 8.0)
+                        self.imageView.alpha = 0
+        }, completion: { (Bool) in
+            // アニメーションが終わったらimageViewを消す
+            self.imageView.removeFromSuperview()
+            self.removeFromParent()
+            let nextVC = HomeViewController()
+            self.addChild(nextVC)
+            self.view.addSubview(nextVC.view!)
+            nextVC.didMove(toParent: self)
+        })
+    }
+    
 
 
 }
