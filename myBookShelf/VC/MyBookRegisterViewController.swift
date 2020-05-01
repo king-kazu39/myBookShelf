76d4b3c8c78b8d670b79225a64b1c82800d9bf6e
Kazuya Nakazato <magmanakazato@gmail.com>
2020-05-01 23:44:25 +0900

    fix #10 書籍登録画面のアップデート

--- myBookShelf/VC/MyBookRegisterViewController.swift
+++ myBookShelf/VC/MyBookRegisterViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class MyBookRegisterViewController: UIViewController, RegisterProtocol {
+class MyBookRegisterViewController: TextFieldViewController, RegisterProtocol,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var picker: UIImagePickerController = UIImagePickerController()
+    var registerView: RegisterView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,16 +19,51 @@ class MyBookRegisterViewController: UIViewController, RegisterProtocol {
         self.title = "MyBookRegister"
         let rect = CGRect(x: 0, y: 0, width: 250, height: 400)
         
-        let registerView = RegisterView(frame: rect)
-            registerView.center = view.center
-        self.view.addSubview(registerView)
-        registerView.delegate = self
-        
+        registerView = RegisterView(frame: rect)
+        registerView?.center = view.center
+        self.view.addSubview(registerView!)
+        registerView?.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setUpNotificationForTextField()
     }
     
     
     func didTapAlertAction(button: UIButton) {
-        showAlertAction()
+        showAlertActionSheet(action1: goToPhoto, action2: gotoCamera)
     }
-
+    
+    private func goToPhoto(){
+        picker.sourceType = .photoLibrary
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            registerView?.imageBtn?.setImage(image, for: .normal)
+        } else {
+            print("error")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func gotoCamera(){
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func didTapRegister(button: UIButton) {
+        showAlert {
+            print("登録します")
+        }
+    }
 }
